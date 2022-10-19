54e84b82707834023175f1a7d14078e9a6a55881
najam1997 <61153309+najam1997@users.noreply.github.com>
2022-10-19 23:27:11 +0500

    Update Download_Images.py

--- Download_Images.py
+++ Download_Images.py
@@ -4,3 +4,14 @@ def Download_Images(url, dir):
  page = ab.open(url)
  soup = BeautifulSoup(html, 'html.parser')
  images = soup.findAll('img')
+ 
+ for image in images:
+  img_file = image['src'].lstrip('http://')
+  img_file = os.path.join(dir,\
+  img_file.replace('/', '_'))
+  print ('[+] Saving ' + str(filename))
+  data = ab.open(image['src']).read()
+  ab.back()
+  save = open(filename, 'wb')
+  save.write(data)
+  save.close()
