57ee9b980b17fc7797159266ae0e105651dd247e
najam1997 <61153309+najam1997@users.noreply.github.com>
2022-10-21 09:47:15 +0500

    Update Download_Images.py

--- Download_Images.py
+++ Download_Images.py
@@ -7,11 +7,34 @@ def Download_Images(url, dir):
  
  for image in images:
   img_file = image['src'].lstrip('http://')
-  img_file = os.path.join(dir,\
-  img_file.replace('/', '_'))
+  img_file = os.path.join(dir, img_file.replace('/', '_'))
   print ('Saving ' + str(filename))
   data = ab.open(image['src']).read()
   ab.back()
-  save = open(filename, 'wb')
+  save = open(img_file, 'ab')
   save.write(data)
   save.close()
+
+def main():
+ parser = optparse.OptionParser()
+ parser.add_option("-u", "--url",
+                  dest = "url",
+                  help = "-u http://example.com")
+ parser.add_option("-d", "--directory",
+                  dest = "directory",
+                  help = "-d /tmp/")
+ (options, args) = parser.parse_args()
+ url = options.url
+ directory = options.directory
+ if url == None or dir == None:
+  print (parser.usage)
+  exit(0)
+ else:
+  try:
+    Download_Images(url, directory)
+  except (Exception, e):
+   print ('Error Downloadimg Images.')
+   print (str(e))
+  
+if __name__ == '__main__':
+ main()
